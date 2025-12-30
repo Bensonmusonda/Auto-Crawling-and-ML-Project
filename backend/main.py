d821a0cac55dc36d5676c35da7a8a25821adf07c
Benson Musonda <70434034+Bensonmusonda@users.noreply.github.com>
2025-12-30 21:07:18 +0200

    add: crawl event monitoring

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 import json
-import redis
+import redis.asyncio as redis
+import asyncio
 from fastapi import FastAPI
 
 from .schemas import CrawlRequest
@@ -7,23 +8,39 @@ from config import Config
 from tasks import run_crawl_task
 
 app = FastAPI()
+cfg = Config()
 
 @app.get("/")
 def get_root():
     return {"Backend is running"}
 
 @app.get("/health/redis")
-def get_redis_health_check():
+async def get_redis_health_check():
     try:
-        r = redis.Redis(host=Config.REDIS_HOST, port=6379, decode_responses=True)
-        r.ping()
+        r = await redis.Redis(host=cfg.REDIS_HOST, port=6379, decode_responses=True)
+        await r.ping()
         return {"redis": "connected", "postgres": "check console"}
     except Exception as e:
         return {"error": str(e)}
     
 @app.post("/crawl")
-def send_crawl_task(config_request: CrawlRequest):
+async def send_crawl_task(config_request: CrawlRequest):
     json_config = config_request.model_dump()
     
     result = run_crawl_task.delay(json_config)
+    asyncio.create_task(monitor_crawl_events())
+
     return {f"started crawl job": result.id}
+
+@app.get("/crawl/monitor")
+async def monitor_crawl_events():
+    async_r = await redis.from_url(f"redis://{cfg.REDIS_HOST}:6379", decode_responses=True)
+    pubsub = async_r.pubsub()
+    await pubsub.subscribe("crawl_events")
+    
+    try:
+        async for message in pubsub.listen():
+            if message and message['type'] == 'message':
+                print(f"Event: {message['data']}")
+    finally:
+        await pubsub.close()
